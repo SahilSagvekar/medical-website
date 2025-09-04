33893f5faf4c44cb4101a6ea31a243acf7e9bc51
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2025-09-04 23:20:19 +0530

    changes

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+// Example: Simple search placeholder and cart interaction
+
+document.querySelector('.cart-btn').addEventListener('click', function() {
+  alert('Cart feature is not implemented in demo.');
+});
+
+document.querySelector('.search-cart input').addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    alert('Search feature is not implemented in demo.');
+  }
+});
